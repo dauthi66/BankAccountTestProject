54ad18d5439e0878b5fe7d5082813b8ee87d0673
Kris <dauthi66@gmail.com>
2022-02-11 18:08:48 -0800

    Created a static class Validator and created tests for it

---
+++ BankAccountTestProjectTests/ValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountTestProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountTestProject.Tests
+{
+    [TestClass()]
+    public class ValidatorTests
+    {
+        [TestMethod()]
+        [DataRow(0, 100, 0)]
+        [DataRow(0, 100, 100)]
+        [DataRow(0, 100, 49.99)]
+        [TestCategory("Validator")]
+        public void IsWithinRangeTest_InclusiveRange_ReturnsTrue(double min, double max, double test)
+        {
+            bool result = Validator.IsWithinRange(min, max, test);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        [DataRow(0, 100, -.01)]
+        [DataRow(0, 100, 100.01)]
+        [TestCategory("Validator")]
+        public void IsWithinRangeTest_ExclusiveRange_ReturnsTrue(double min, double max, double test)
+        {
+            bool result = Validator.IsWithinRange(min, max, test);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
